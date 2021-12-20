46343f5e66ee2c2ff3571f969e5319a438030245
Benoît Puech <benoit.puech@yahoo.fr>
2021-12-20 07:10:13 +0100

    Contains pages content without any other component used

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>À Propos</h1>
+      <br />
+      <p>
+        Ipsum ut cillum incididunt laboris nisi nostrud sint nostrud velit
+        Lorem. Amet aliquip cupidatat irure Lorem labore ea nostrud anim mollit
+        nostrud nisi. Et consectetur pariatur adipisicing duis elit irure esse
+        deserunt anim cupidatat voluptate.
+        <br />
+        Magna ad ipsum Lorem aliqua id quis est ullamco fugiat amet. Cupidatat
+        voluptate consectetur velit occaecat eiusmod exercitation consequat quis
+        qui culpa velit ipsum velit. Velit qui laborum quis nostrud ut ad labore
+        do sint veniam. Est irure incididunt dolor reprehenderit. Sit qui do
+        nulla duis. Sint mollit cupidatat dolor mollit nulla. Sint veniam cillum
+        nostrud amet sunt occaecat tempor.
+      </p>
+    </div>
+  );
+};
+
+export default About;
